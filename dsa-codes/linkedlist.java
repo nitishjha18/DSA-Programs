521cbec75a7308fb7fc2d9c93156cfeb3305188b
nitishjha18 <nitishjha1100@gmail.com>
2024-09-03 14:31:06 +0530

    Remove a loop/cycle in ll

--- dsa-codes/linkedlist.java
+++ dsa-codes/linkedlist.java
@@ -235,12 +235,45 @@ public class linkedlist {
         return false;          // cycle doesn't exist
     }
 
+    public static void removeCycle(){
+
+//        detetect cycle
+        Node slow = head;
+        Node fast = head;
+        boolean cycle = false;
+        while (fast != null && fast.next != null){
+            slow = slow.next;      // +1
+            fast = fast.next.next; // +2
+            if (fast == slow){
+                cycle = true;
+                break;
+            }
+        }
+        if (cycle == false){
+            return;
+        }
+
+//          Find meeting point
+        slow = head;
+        Node prev = null;     // last node
+        while (slow != fast){
+            prev = fast;
+            slow = slow.next;    // +1
+            fast = fast.next;    // +1
+        }
+//          remove cycle --> last.next = null
+        prev.next = null;
+    }
+
     public static void main (String args[]){
         head = new Node(1);
-        head.next = new Node(2);
+        Node temp = new Node(2);
+        head.next = temp;
         head.next.next = new Node(3);
-        head.next.next.next = head;
-//      1->2->3->1
+        head.next.next.next = temp;
+//        1->2->3->2
+        System.out.println(isCycle());
+        removeCycle();
         System.out.println(isCycle());
 
     }
