cae843d4235a0a75620e3298384de078559ff2e9
nitishjha18 <nitishjha1100@gmail.com>
2024-10-30 18:18:31 +0530

    Valid Parenthesis

---
+++ StackQues.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+public class StackQues {
+    /*public static void stockSpan(int stocks[], int span[]){
+        Stack<Integer> s = new Stack<>();
+        span[0] = 1;
+        s.push(0);
+        for (int i=1; i<stocks.length; i++){
+            int currPrice = stocks[i];
+            while(!s.isEmpty() && currPrice>stocks[s.peek()]){
+                s.pop();
+            }
+            if (s.isEmpty()){
+                span[i] = i+1;
+            }else {
+                int prevHigh = s.peek();
+                span[i] = i-prevHigh;
+            }
+            s.push(i);
+        }}
+        public static void main(String args[]){
+            int stocks[] = {100,80,60,70,60,85,100};
+            int span[]  = new int[stocks.length];
+            stockSpan(stocks,span);
+            for (int i=0; i<span.length; i++){
+                System.out.println(span[i] + " ");
+            }
+
+    }*/
+//    Q Valid paranthesis
+    public static boolean isValid(String str) {
+        Stack<Character> s = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {   // opening
+                s.push(ch);
+            } else {
+                // closing
+                if (s.isEmpty()) {
+                    return false;
+                }
+                if ((s.peek() == '(' && ch == ')')       //  ()
+                    ||(s.peek() == '{' && ch == '}')     //  {}
+                    || (s.peek() == '[' && ch == ']')) { //  []
+                    s.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+        if (s.isEmpty()){
+            return true;
+        }else {
+            return false;
+        }
+    }
+    public static void main(String args[]){
+        String str = "[{()}(){}]";  // true
+        System.out.println(isValid(str));
+    }
+}
