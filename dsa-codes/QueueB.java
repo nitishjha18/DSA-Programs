1e84541198b8d072c645b51acfce876b0481cf24
nitishjha18 <nitishjha1100@gmail.com>
2024-11-14 20:24:45 +0530

    Implementing Queue using LL

--- dsa-codes/QueueB.java
+++ dsa-codes/QueueB.java
@@ -1,5 +1,5 @@
 public class QueueB {
-    static class Queue {
+    /*static class Queue {
         static int arr[];
         static int size;
         static int rear;
@@ -46,18 +46,69 @@ public class QueueB {
             }
             return arr[0];
         }
+    }*/
+    static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
     }
+    static class Queue {
+        static  Node head = null;
+        static Node tail = null;
 
-    public static void main(String args[]) {
-        Queue q = new Queue(5);
-        q.add(1);
-        q.add(2);
-        q.add(3);
-        while(!q.isEmpty()){
-            System.out.println(q.peek());
-            q.remove();
+        public boolean isEmpty() {
+            return head == null && tail == null;
         }
 
-    }
+        //    add
+        public void add(int data) {
+            Node newNode = new Node(data);
+            if (head == null) {
+                head = tail = newNode;
+                return;
+            }
+            tail.next = newNode;
+            tail = newNode;
+        }
 
-}
+        //        remove
+        public int remove() {
+            if (isEmpty()) {
+                System.out.println("empty queue");
+                return -1;
+            }
+            int front = head.data;
+//            single element
+            if (tail == head) {
+                tail = head = null;
+            } else {
+                head = head.next;
+            }
+            return front;
+        }
+
+        //        peek
+        public int peek() {
+            if (isEmpty()) {
+                System.out.println("empty queue");
+                return -1;
+            }
+            return head.data;
+        }
+
+        public static void main(String[] args) {
+            Queue q = new Queue();
+            q.add(1);
+            q.add(2);
+            q.add(3);
+            while (!q.isEmpty()) {
+                System.out.println(q.peek());
+                q.remove();
+            }
+
+        }
+
+    }}
