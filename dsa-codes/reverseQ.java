320053fa4a97178ca31018542b4095dc061ab985
nitishjha18 <nitishjha1100@gmail.com>
2025-01-09 23:24:06 +0530

    Queue series

---
+++ dsa-codes/reverseQ.java
@@ -0,0 +1,32 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class reverseQ {
+    public static void reverse(Queue<Integer>q){
+        Stack<Integer> s = new Stack<>();
+        while(!q.isEmpty()){
+            s.push(q.remove());
+        }
+        while(!s.isEmpty()){
+            q.add(s.pop());
+        }
+    }
+    public static void main(String args[]){
+            Queue<Integer> q = new LinkedList<>();
+            q.add(1);
+            q.add(2);
+            q.add(3);
+            q.add(4);
+            q.add(5);
+           reverse(q);
+//      print Q
+            while(!q.isEmpty()){
+                System.out.print(q.remove() + "  ");
+            }
+        System.out.println();
+        }
+    }
+
